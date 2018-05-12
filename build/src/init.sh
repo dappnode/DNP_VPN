c1f5a99e009ff44fb2c2c3611164b3c6d167aa4c
Eduardo Antuña Díez <eduadiez@gmail.com>
2018-05-12 09:56:27 +0200

    Moved to 172.33.0.0/16 network

--- build/src/init.sh
+++ build/src/init.sh
@@ -45,9 +45,9 @@ export VPN_USER=dappnode_admin
 export VPN_PASSWORD="$(LC_CTYPE=C tr -dc 'A-HJ-NPR-Za-km-z2-9' < /dev/urandom | head -c 16)"
 export VPN_IPSEC_PSK="$(LC_CTYPE=C tr -dc 'A-HJ-NPR-Za-km-z2-9' < /dev/urandom | head -c 16)"
 export VPN_PASSWORD_ENC=$(openssl passwd -1 "$VPN_PASSWORD")
-export L2TP_NET=${VPN_L2TP_NET:-'192.168.42.0/24'}
-export L2TP_LOCAL=${VPN_L2TP_LOCAL:-'192.168.42.1'}
-export L2TP_POOL=${VPN_L2TP_POOL:-'192.168.42.10-192.168.42.250'}
+export L2TP_NET=${VPN_L2TP_NET:-'172.33.0.0/16'}
+export L2TP_LOCAL=${VPN_L2TP_LOCAL:-'172.33.11.1'}
+export L2TP_POOL=${VPN_L2TP_POOL:-'172.33.100.1-172.33.255.254'}
 export XAUTH_NET=${VPN_XAUTH_NET:-'192.168.43.0/24'}
 export XAUTH_POOL=${VPN_XAUTH_POOL:-'192.168.43.10-192.168.43.250'}
 export DNS_SRV1=${VPN_DNS_SRV1:-'8.8.8.8'}
