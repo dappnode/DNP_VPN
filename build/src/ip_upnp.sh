5f77f6f19d3dcfa7751336f4d447c12f6f82505e
Unknown <guillem.lion.benosa@gmail.com>
2018-06-28 00:18:16 +0200

    Made the external / internal IP files non-mandatory

--- build/src/ip_upnp.sh
+++ build/src/ip_upnp.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Fallback null response in case of error
-echo "" > $EXTERNAL_IP_FILE_PATH 
-echo "" > $INTERNAL_IP_FILE_PATH
-
 export IMAGE=$(docker inspect DAppNodeCore-vpn.dnp.dappnode.eth -f '{{.Config.Image}}')
 export ExternalIP=$(docker run --rm --net=host ${IMAGE} upnpc -l | awk -F'= '  '/ExternalIPAddress/{print $2}')
 export InternalIP=$(docker run --rm --net=host ${IMAGE} ip route get 1  | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
--- build/src/modules/fetchVPNparameters.js
+++ build/src/modules/fetchVPNparameters.js
@@ -8,40 +8,37 @@ const VPN_NAME_FILE_PATH = process.env.DEV ? './test/name' : process.env.VPN_NAM
 const INT_IP_FILE_PATH = process.env.DEV ? './test/internal-ip' : process.env.INTERNAL_IP_FILE_PATH
 const EXT_IP_FILE_PATH = process.env.DEV ? './test/external-ip' : process.env.EXTERNAL_IP_FILE_PATH
 
-let maxAttempts = 120;
+let maxAttempts = 10;
 
 async function fetchVPNparameters() {
 
+  // The second parameter is a fallback value
+  // Not providing if enforces the existance of the file
   const IP = await fetchVPN_PARAMETER(VPN_IP_FILE_PATH)
   const PSK = await fetchVPN_PARAMETER(VPN_PSK_FILE_PATH)
-  const INT_IP = await fetchVPN_PARAMETER(INT_IP_FILE_PATH)
-  const EXT_IP = await fetchVPN_PARAMETER(EXT_IP_FILE_PATH)
-
-  // The existence of this file is not crucial to the system
-  // It it doesn't exist fallback to a default name
-  let NAME
-  if (fs.existsSync(VPN_NAME_FILE_PATH)) {
-    NAME = await fetchVPN_PARAMETER(VPN_NAME_FILE_PATH)
-  } else {
-    NAME = 'DAppNode_server'
-  }
+  const INT_IP = await fetchVPN_PARAMETER(INT_IP_FILE_PATH, "")
+  const EXT_IP = await fetchVPN_PARAMETER(EXT_IP_FILE_PATH, "")
+  const NAME = await fetchVPN_PARAMETER(VPN_NAME_FILE_PATH, 'DAppNode_server')
 
   return { IP, PSK, INT_IP, EXT_IP, NAME }
-
 }
 
 
-async function fileToExist(FILE_PATH) {
+async function fileToExist(FILE_PATH, fallbackValue) {
     for (let i = 0; i < maxAttempts; i++) {
       if (fs.existsSync(FILE_PATH)) return
       await pauseSync(500)
     }
-    throw 'File not existent after #' + maxAttempts + ' attempts, path: ' + FILE_PATH
+    if (fallbackValue) {
+      console.log('File '+FILE_PATH+' not found (#' + maxAttempts + ' tries) - WARN: Using fallback value: '+fallbackValue)
+      return fallbackValue
+    }
+    throw 'File '+FILE_PATH+' not found (#' + maxAttempts + ' tries) - ERROR: file is mandatory'
 }
 
 
-const fetchVPN_PARAMETER = (FILE_PATH) => 
-  fileToExist(FILE_PATH)
+const fetchVPN_PARAMETER = (FILE_PATH, fallbackValue = false) => 
+  fileToExist(FILE_PATH, fallbackValue)
   .then(() => readFile(FILE_PATH, 'utf-8'))
   .then(data => String(data).trim())
 
