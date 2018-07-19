cc850e8b0ee96b8e83e766f47b640b087e88e780
Unknown <guillem.lion.benosa@gmail.com>
2018-07-19 19:53:18 +0200

    Added travis ci lint coveralls

--- build/src/src/utils/credentialsFile.test.js
+++ build/src/src/utils/credentialsFile.test.js
@@ -1,38 +1,35 @@
-const chai = require('chai')
+const chai = require('chai');
 
 chai.should();
 
-const credentialsFile = require('./credentialsFile')
+const credentialsFile = require('./credentialsFile');
 
 describe('Util: credentialsFile', function() {
-
   const credentialsArray = [
     {
       name: 'SUPERadmin',
       password: 'MockPass2',
-      ip: '172.33.10.1'
+      ip: '172.33.10.1',
     },
     {
       name: 'MockName',
       password: 'MockPass',
-      ip: '172.33.100.123'
-    }
-  ]
+      ip: '172.33.100.123',
+    },
+  ];
 
   it('should write the file', (done) => {
     credentialsFile.write(credentialsArray)
     .then(() => {
-      done()
-    })
+      done();
+    });
   });
 
   it('should read the file', (done) => {
     credentialsFile.fetch()
-    .then((credentialsArray_RES) => {
-      credentialsArray_RES.should.deep.equal(credentialsArray)
-      done()
-    })
-
+    .then((credentialsArrayRes) => {
+      credentialsArrayRes.should.deep.equal(credentialsArray);
+      done();
+    });
   });
-
 });
