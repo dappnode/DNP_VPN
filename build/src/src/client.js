7d1cc346cecb972db59e85f7e36c92d82a070524
Unknown <guillem.lion.benosa@gmail.com>
2018-07-24 13:01:06 +0200

    Linted code

--- build/src/src/client.js
+++ build/src/src/client.js
@@ -1,108 +1,97 @@
-const autobahn = require('autobahn')
-const fs = require('file-system')
+const autobahn = require('autobahn');
 
 // import calls
-const createAddDevice = require('./calls/createAddDevice')
-const createRemoveDevice = require('./calls/createRemoveDevice')
-const createToggleAdmin = require('./calls/createToggleAdmin')
-const createListDevices = require('./calls/createListDevices')
-const createGetParams = require('./calls/createGetParams')
-const createStatusUPnP = require('./calls/createStatusUPnP')
-const createStatusExternalIp = require('./calls/createStatusExternalIp')
+const createAddDevice = require('./calls/createAddDevice');
+const createRemoveDevice = require('./calls/createRemoveDevice');
+const createToggleAdmin = require('./calls/createToggleAdmin');
+const createListDevices = require('./calls/createListDevices');
+const createGetParams = require('./calls/createGetParams');
+const createStatusUPnP = require('./calls/createStatusUPnP');
+const createStatusExternalIp = require('./calls/createStatusExternalIp');
 
 // import dependencies
-const credentialsFile = require('./utils/credentialsFile')
-const generate = require('./utils/generate')
-const createLogAdminCredentials = require('./modules/createLogAdminCredentials')
-const fetchVPNparameters = require('./modules/fetchVPNparameters')
+const credentialsFile = require('./utils/credentialsFile');
+const generate = require('./utils/generate');
+const createLogAdminCredentials = require('./modules/createLogAdminCredentials');
+const fetchVPNparameters = require('./modules/fetchVPNparameters');
 
 // Initialize dependencies
-const params = {}
-const logAdminCredentials = createLogAdminCredentials(credentialsFile, generate)
+const params = {};
+const logAdminCredentials = createLogAdminCredentials(credentialsFile, generate);
 
 // Initialize calls
-const addDevice = createAddDevice(credentialsFile, generate)
-const removeDevice = createRemoveDevice(credentialsFile)
-const toggleAdmin = createToggleAdmin(credentialsFile)
-const listDevices = createListDevices(credentialsFile, generate, params)
-const getParams = createGetParams(params)
-const statusUPnP = createStatusUPnP(params, fetchVPNparameters)
-const statusExternalIp = createStatusExternalIp(params, fetchVPNparameters)
+const addDevice = createAddDevice(credentialsFile, generate);
+const removeDevice = createRemoveDevice(credentialsFile);
+const toggleAdmin = createToggleAdmin(credentialsFile);
+const listDevices = createListDevices(credentialsFile, generate, params);
+const getParams = createGetParams(params);
+const statusUPnP = createStatusUPnP(params, fetchVPNparameters);
+const statusExternalIp = createStatusExternalIp(params, fetchVPNparameters);
 
-const URL = 'ws://my.wamp.dnp.dappnode.eth:8080/ws'
-const REALM = 'dappnode_admin'
+const URL = 'ws://my.wamp.dnp.dappnode.eth:8080/ws';
+const REALM = 'dappnode_admin';
 
 
-///////////////////////////////
+// /////////////////////////////
 // Setup crossbar connection //
-///////////////////////////////
+// /////////////////////////////
 
 
-const connection = new autobahn.Connection({ url: URL, realm: REALM })
-const SUCCESS_MESSAGE = '---------------------- \n procedure registered'
-const ERROR_MESSAGE = '------------------------------ \n failed to register procedure '
+const connection = new autobahn.Connection({url: URL, realm: REALM});
 
-connection.onopen = function (session, details) {
+connection.onopen = function(session, details) {
+  console.log('CONNECTED to DAppnode\'s WAMP '+
+      '\n   url '+URL+
+      '\n   realm: '+REALM+
+      '\n   session ID: '+details.authid);
 
-  console.log("CONNECTED to DAppnode's WAMP "+
-      "\n   url "+URL+
-      "\n   realm: "+REALM+
-      "\n   session ID: "+details.authid)
+  register(session, 'ping.vpn.dnp.dappnode.eth', (x) => x);
+  register(session, 'getParams.vpn.dappnode.eth', getParams);
+  register(session, 'addDevice.vpn.dnp.dappnode.eth', addDevice);
+  register(session, 'removeDevice.vpn.dnp.dappnode.eth', removeDevice);
+  register(session, 'toggleAdmin.vpn.dnp.dappnode.eth', toggleAdmin);
+  register(session, 'listDevices.vpn.dnp.dappnode.eth', listDevices);
+  register(session, 'statusUPnP.vpn.dnp.dappnode.eth', statusUPnP);
+  register(session, 'statusExternalIp.vpn.dnp.dappnode.eth', statusExternalIp);
+};
 
-  register(session, 'ping.vpn.dnp.dappnode.eth', x => x)
-  register(session, 'getParams.vpn.dappnode.eth', getParams)
-  register(session, 'addDevice.vpn.dnp.dappnode.eth', addDevice)
-  register(session, 'removeDevice.vpn.dnp.dappnode.eth', removeDevice)
-  register(session, 'toggleAdmin.vpn.dnp.dappnode.eth', toggleAdmin)
-  register(session, 'listDevices.vpn.dnp.dappnode.eth', listDevices)
-  register(session, 'statusUPnP.vpn.dnp.dappnode.eth', statusUPnP)
-  register(session, 'statusExternalIp.vpn.dnp.dappnode.eth', statusExternalIp)
-
-}
-
-connection.onclose = function (reason, details) {
-  console.log('[client.js connection.onclose] reason: '+reason+' details '+JSON.stringify(details))
+connection.onclose = function(reason, details) {
+  console.log('[client.js connection.onclose] reason: '+reason+' details '+JSON.stringify(details));
 };
 
 
-////////////////////////////////
+// //////////////////////////////
 //  Open crossbar connection  //
-////////////////////////////////
+// //////////////////////////////
 
 
-start()
+start();
 
-async function start () {
+async function start() {
+  console.log('Waiting for credentials files to exist');
+  params.VPN = await fetchVPNparameters();
 
-  console.log('Waiting for credentials files to exist')
-  params.VPN = await fetchVPNparameters()
-  
-  console.log('VPN credentials fetched - \n  ' 
-    + Object.keys(params.VPN).map(name => name+": "+params.VPN[name]).join('\n  '))
+  console.log('VPN credentials fetched - \n  '
+    + Object.keys(params.VPN).map((name) => name+': '+params.VPN[name]).join('\n  '));
 
-  logAdminCredentials(params.VPN)
+  logAdminCredentials(params.VPN);
 
   console.log('Attempting to connect to.... \n'
     +'   url: '+connection._options.url+'\n'
-    +'   realm: '+connection._options.realm)
-  connection.open()
-
+    +'   realm: '+connection._options.realm);
+  connection.open();
 }
 
 
-///////////////////////////////
+// /////////////////////////////
 // Connection helper functions
 
 
 function register(session, event, handler) {
-
-  const SUCCESS_MESSAGE = '---------------------- \n procedure registered'
-  const ERROR_MESSAGE = '------------------------------ \n failed to register procedure '
-
   return session.register(event, wrapErrors(handler)).then(
-    function (reg) { console.log(SUCCESS_MESSAGE) },
-    function (err) { console.log(ERROR_MESSAGE, err) }
-  )
+    function(reg) {console.log('CROSSBAR: registered '+event);},
+    function(err) {console.log('CROSSBAR: error registering '+event+'. Error message: '+err.error);}
+  );
 }
 
 
--- build/src/src/getAdminCredentials.js
+++ build/src/src/getAdminCredentials.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 // import dependencies
-const createLogAdminCredentials = require('./modules/createLogAdminCredentials')
-const credentialsFile = require('./utils/credentialsFile')
-const generate = require('./utils/generate')
-const fetchVPNparameters = require('./modules/fetchVPNparameters')
+const createLogAdminCredentials = require('./modules/createLogAdminCredentials');
+const credentialsFile = require('./utils/credentialsFile');
+const generate = require('./utils/generate');
+const fetchVPNparameters = require('./modules/fetchVPNparameters');
 
 // Initialize dependencies
-const logAdminCredentials = createLogAdminCredentials(credentialsFile, generate)
+const logAdminCredentials = createLogAdminCredentials(credentialsFile, generate);
 
-start()
+start();
 
 async function start() {
-  const VPN = await fetchVPNparameters()
-  logAdminCredentials(VPN)
+  const VPN = await fetchVPNparameters();
+  logAdminCredentials(VPN);
 }
