29bf012df05aeaffe72fffa4e1ca33610c1b263c
Unknown <guillem.lion.benosa@gmail.com>
2018-07-27 13:55:15 +0200

    Extended the log admin credentials message

--- build/src/src/calls/createStatusUPnP.js
+++ build/src/src/calls/createStatusUPnP.js
@@ -23,27 +23,33 @@ function createStatusUPnP(params, fetchVPNparameters) {
 const getStatus = (VPN) => {
   const {IP, EXT_IP, INT_IP} = VPN;
 
-  if (EXT_IP && EXT_IP.length) {return ({
-    openPorts: true,
-    UPnP: true,
-    msg: 'UPnP device available',
-  });}
+  if (EXT_IP && EXT_IP.length) {
+    return ({
+      openPorts: true,
+      UPnP: true,
+      msg: 'UPnP device available',
+    });
+  }
 
   else if (
     INT_IP && INT_IP.length
     && IP && IP.length
     && INT_IP === IP
-  ) {return ({
-    openPorts: false,
-    UPnP: true,
-    msg: 'Cloud service',
-  });}
+  ) {
+    return ({
+      openPorts: false,
+      UPnP: true,
+      msg: 'Cloud service',
+    });
+  }
 
-  else {return ({
-    openPorts: true,
-    UPnP: false,
-    msg: 'UPnP not available. Turn it on or open ports manually',
-  });}
+  else {
+    return ({
+      openPorts: true,
+      UPnP: false,
+      msg: 'UPnP not available. Turn it on or open ports manually',
+    });
+  }
 };
 
 const runUpnpScript = () => {
