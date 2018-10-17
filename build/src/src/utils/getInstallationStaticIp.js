04ac20f3080b18a904c627ee2a266b717ce15473
Unknown <guillem.lion.benosa@gmail.com>
2018-10-17 10:44:51 +0200

    Make sure the ip is valid

--- build/src/src/utils/getInstallationStaticIp.js
+++ build/src/src/utils/getInstallationStaticIp.js
@@ -2,6 +2,7 @@ const fs = require('file-system');
 const {promisify} = require('util');
 const readFileAsync = promisify(fs.readFile);
 const logs = require('../logs.js')(module);
+const ipRegex = require('ip-regex');
 
 const {INSTALLATION_STATIC_IP} = process.env;
 
@@ -12,6 +13,7 @@ function getInstallationStaticIp() {
     .then((data) => String(data).trim())
     // If the file is empty return null
     .then((data) => data.length ? data : null)
+    .then((ip) => ipRegex({exact: true}).test(ip))
     .catch((err) => {
         logs.error(`Error reading INSTALLATION_STATIC_IP ${INSTALLATION_STATIC_IP}: ${err.stack || err.message}`);
         return null;
