47c8412c7b55cb64a96616382eb495f4fcffd02e
Unknown <guillem.lion.benosa@gmail.com>
2019-02-02 08:19:09 +0100

    Update functions to use new params

--- build/src/src/calls/getParams.js
+++ build/src/src/calls/getParams.js
@@ -5,7 +5,13 @@ async function getParams() {
     const result = {
         ip: _db.ip,
         name: _db.name,
+        // Add status parameters
+        upnpAvailable: _db.upnpAvailable,
+        noNatLoopback: _db.noNatLoopback,
+        alertToOpenPorts: _db.alertToOpenPorts,
+        internalIp: _db.internalIp,
     };
+    // Don't append the domain when the static IP is active
     const staticIp = _db.staticIp;
     const domain = _db.domain;
     if (staticIp) {
--- build/src/src/loginMsg/generateLoginMsg.js
+++ build/src/src/loginMsg/generateLoginMsg.js
@@ -1,18 +1,25 @@
 const qrcode = require('qrcode-terminal');
+const db = require('../db');
 
 async function generateLoginMsg(url) {
   let msg = '\n\n';
 
   // Show the QR code
   // Wraps qrcode library's callback style into a promise
-  if (!url) throw Error('generateLoginMsg: url is empty or not defined')
+  if (!url) throw Error('generateLoginMsg: url is empty or not defined');
   msg += await getQrCodeString(url);
 
   // Show credentials
   /* eslint-disable max-len */
-  msg += `
-To connect to your DAppNode scan the QR above or copy/paste link below into your browser:
+  msg += `\n To connect to your DAppNode scan the QR above or copy/paste link below into your browser:
   ${url}\n`;
+
+  if (await db.get('alertToOpenPorts')) {
+    msg += `\n ALERT: You may not be able to connect. Turn your router's UPnP on or open the VPN port (1194/udp) manually`;
+  }
+  if (await db.get('noNatLoopback')) {
+    msg += `\n ALERT: NAT-Loopback is disabled. If you are connecting from the same network as your DAppNode use the internal IP: ${await db.get('internalIp')}`;
+  }
 
   return msg;
 }
@@ -22,29 +29,6 @@ function getQrCodeString(data) {
     qrcode.setErrorLevel('S');
     qrcode.generate(data, resolve);
   });
-}
-
-function parseUpnpStatus(openPorts, upnpAvailable) {
-  if (openPorts && !upnpAvailable) {
-    // upnpStatus: {
-    //   openPorts: true, // true => ports have to be opened
-    //   upnp: true, // true => UPnP is able to open them automatically
-    // },
-    return '\n ALERT: You may not be able to connect. '
-      +'Turn your router\'s UPnP on or open the VPN port (1194/udp) manually';
-  } else {
-    return '';
-  }
-}
-
-function parsePublicIpStatus(externalIpResolves, internalIp) {
-  if (!externalIpResolves) {
-    return '\n ALERT: (NAT-Loopback disable) '
-      +'If you are connecting from the same network as your DAppNode '
-      +'use the internal IP: '+internalIp;
-  } else {
-    return '';
-  }
 }
 
 module.exports = generateLoginMsg;
