9bda0af4ab5804cb04d51fe06031f835a857bc00
dapplion <guillem.lion.benosa@gmail.com>
2020-07-10 16:46:48 +0200

    Clean loginMsg code

--- build/src/src/vpncli.ts
+++ build/src/src/vpncli.ts
@@ -1,21 +1,15 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-console */
-
+import url from "url";
 import cmd from "commander";
 import chalk from "chalk";
-import { generateAndWriteLoginMsg } from "./loginMsg";
 import prettyjson from "prettyjson";
 import { getRpcCall } from "./api/getRpcCall";
+import { API_PORT } from "./params";
 
-const urlDefault = "http://localhost:3000";
-
-const adminUser = process.env.DEFAULT_ADMIN_USER
-  ? process.env.DEFAULT_ADMIN_USER
-  : "dappnode_admin";
+/* eslint-disable no-console */
 
 cmd
-  .option("--url <url>", `VPN RPC API url, default: ${urlDefault}`)
   .option("ls", "List devices.")
   .option("get <id>", "Generate device URL to download config file.")
   .option("add <id>", "Add device.")
@@ -36,8 +30,16 @@ if (process.argv.length <= 2) {
  */
 (async function(): Promise<void> {
   try {
-    const api = getRpcCall(cmd.url || urlDefault);
+    const vpnRpcApiUrl = url.format({
+      protocol: "http",
+      hostname: "127.0.0.1",
+      port: API_PORT
+    });
+    // Initialize an RPC client connecting to the VPN RPC server
+    const api = getRpcCall(vpnRpcApiUrl);
+
     const id = cmd.add || cmd.get || cmd.rm || cmd.toggle || cmd.reset;
+
     if (cmd.add) {
       await api.addDevice({ id });
       console.log(chalk.green(`Added device ${id}`));
