65cc0970cd880ddc64f2ac5f894566e4e5b3ece5
dapplion <guillem.lion.benosa@gmail.com>
2020-07-15 10:35:02 +0200

    Do not allow to delete MASTER_ADMIN_NAME

--- build/src/src/openvpn/removeClient.ts
+++ build/src/src/openvpn/removeClient.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { deleteTokenForId } from "../credentials";
 import { shell } from "../utils/shell";
+import { MASTER_ADMIN_NAME } from "../params";
 
 const revokeCommand = "/usr/local/bin/ovpn_revokeclient";
 
@@ -8,6 +9,9 @@ const revokeCommand = "/usr/local/bin/ovpn_revokeclient";
  * @param id "new-device"
  */
 export async function removeClient(id: string): Promise<void> {
+  if (id === MASTER_ADMIN_NAME)
+    throw Error("Cannot remove the master admin user");
+
   try {
     // Revoke first to save in CRL
     await shell(`${revokeCommand} ${id}`);
