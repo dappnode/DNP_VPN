092bf09476e213c5d1a54abc34d48628210e65c9
dapplion <guillem.lion.benosa@gmail.com>
2020-07-16 14:38:14 +0200

    Change commander for yargs in vpncli

--- build/src/src/vpncli.ts
+++ build/src/src/vpncli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
+import yargs, { CommandBuilder } from "yargs";
 import url from "url";
-import cmd from "commander";
 import chalk from "chalk";
 import prettyjson from "prettyjson";
 import { getRpcCall } from "./api/getRpcCall";
@@ -9,61 +9,81 @@ import { API_PORT } from "./params";
 
 /* eslint-disable no-console */
 
-cmd
-  .option("ls", "List devices.")
-  .option("get <id>", "Generate device URL to download config file.")
-  .option("add <id>", "Add device.")
-  .option("rm <id>", "Remove device.")
-  .option("toggle <id>", "Give/remove admin rights to device.")
-  .option("reset <id>", "Reset device credentials.")
-  .parse(process.argv);
+const vpnRpcApiUrl = url.format({
+  protocol: "http",
+  hostname: "127.0.0.1",
+  port: API_PORT,
+  pathname: "rpc"
+});
+// Initialize an RPC client connecting to the VPN RPC server
+const api = getRpcCall(vpnRpcApiUrl);
 
-if (process.argv.length <= 2) {
-  console.log(`Usage: ${chalk.yellow("vpncli [option]")}
-       ${chalk.yellow("vpncli --help")}\t to view available options\n`);
-  process.exit(1);
-}
+const idArg: CommandBuilder<{}, { id: string }> = yargs =>
+  yargs.positional("id", {
+    describe: "Device id",
+    type: "string",
+    demandOption: true
+  });
 
-/**
- * Parses a commander instance
- * Returns null or throws an error
- */
-(async function(): Promise<void> {
-  try {
-    const vpnRpcApiUrl = url.format({
-      protocol: "http",
-      hostname: "127.0.0.1",
-      port: API_PORT,
-      pathname: "rpc"
-    });
-    // Initialize an RPC client connecting to the VPN RPC server
-    const api = getRpcCall(vpnRpcApiUrl);
-
-    const id = cmd.add || cmd.get || cmd.rm || cmd.toggle || cmd.reset;
-
-    if (cmd.add) {
-      await api.addDevice({ id });
-      console.log(chalk.green(`Added device ${id}`));
-    } else if (cmd.get) {
-      const { url } = await api.getDeviceCredentials({ id });
-      console.log(chalk.green(`Credentials generated for ${id}: ${url}`));
-    } else if (cmd.ls) {
+yargs
+  .usage(`Usage: vpncli <command> [options]`)
+  .alias("h", "help")
+  .alias("v", "version")
+  // blank scriptName so that help text doesn't display the cli name before each command
+  .scriptName("")
+  .demandCommand(1)
+  .command({
+    command: "ls",
+    describe: "List devices.",
+    handler: async () => {
       const devices = await api.listDevices();
       console.log(prettyjson.render(devices));
-    } else if (cmd.rm) {
+    }
+  })
+  .command({
+    command: "get",
+    describe: "Generate device URL to download config file.",
+    builder: idArg,
+    handler: async ({ id }) => {
+      const { url } = await api.getDeviceCredentials({ id });
+      console.log(chalk.green(`Credentials generated for ${id}: ${url}`));
+    }
+  })
+  .command({
+    command: "add",
+    describe: "Add device.",
+    builder: idArg,
+    handler: async ({ id }) => {
+      await api.addDevice({ id });
+      console.log(chalk.green(`Added device ${id}`));
+    }
+  })
+  .command({
+    command: "rm",
+    describe: "Remove device.",
+    builder: idArg,
+    handler: async ({ id }) => {
       await api.removeDevice({ id });
       console.log(chalk.green(`Removed device ${id}`));
-    } else if (cmd.toggle) {
+    }
+  })
+  .command({
+    command: "toggle",
+    describe: "Give/remove admin rights to device.",
+    builder: idArg,
+    handler: async ({ id }) => {
       await api.toggleAdmin({ id });
       console.log(chalk.green(`Toggled admin status of ${id}`));
-    } else if (cmd.reset) {
+    }
+  })
+  .command({
+    command: "reset",
+    describe: "Reset device credentials.",
+    builder: idArg,
+    handler: async ({ id }) => {
       await api.resetDevice({ id });
       console.log(chalk.green(`Reset device ${id}`));
-    } else {
-      console.log(chalk.yellow("Command unknown!"));
     }
-  } catch (e) {
-    console.error(chalk.red(e));
-    process.exit(1);
-  }
-})();
+  })
+  // Run CLI
+  .parse();
