ad926a69e1a5789a6621e073f4a48dcc5217c891
Pablo <mendez4a@gmail.com>
2020-12-11 08:54:29 +0100

    fetch internalIP from dappmanager in getClient()

--- build/src/src/openvpn/getClient.ts
+++ build/src/src/openvpn/getClient.ts
@@ -1,3 +1,11 @@
+import got from "got/dist/source";
+import { env } from "yargs";
+import { logs } from "../logs";
+import {
+  GLOBAL_ENVS_KEYS,
+  dappmanagerApiUrlGlobalEnvs,
+  GLOBAL_ENVS
+} from "../params";
 import { shell } from "../utils/shell";
 
 const fetchCredsCommand = "/usr/local/bin/ovpn_getclient";
@@ -7,9 +15,29 @@ const fetchCredsCommand = "/usr/local/bin/ovpn_getclient";
  * @param id "new-device"
  */
 export async function getClient(id: string): Promise<string> {
+  const internalIp = await getInternalIp();
   try {
-    return await shell(`${fetchCredsCommand} ${id}`);
+    return await shell(`${fetchCredsCommand} ${id}`, {
+      env: { DAPPNODE_INTERNAL_IP: internalIp }
+    });
   } catch (err) {
     throw Error(`Error retrieving client ${id}: ${err.message}`);
+  }
+}
+
+async function getInternalIp(): Promise<string> {
+  const internalIpFromEnv = process.env[GLOBAL_ENVS.INTERNAL_IP];
+  try {
+    const internalIp = await got(GLOBAL_ENVS_KEYS.INTERNAL_IP, {
+      throwHttpErrors: true,
+      prefixUrl: dappmanagerApiUrlGlobalEnvs
+    })
+      .text()
+      .then(res => res.trim());
+
+    return internalIp;
+  } catch (e) {
+    logs.warn(`Error getting internal IP from DAPPMANAGER`, e);
+    return internalIpFromEnv || "";
   }
 }
