88176f89d4adef3394b77e4650de86567f536501
dappnodedev <diego@dappnode.io>
2023-12-26 16:31:09 +0100

    Avoid fetching internal IP if env exists

--- src/src/dappmanager/fetchInternalIp.ts
+++ src/src/dappmanager/fetchInternalIp.ts
@@ -1,13 +1,26 @@
 import ip from "ip";
 import got from "got";
 import { logs } from "../logs";
-import { dappmanagerApiUrlGlobalEnvs, GLOBAL_ENVS_KEYS } from "../params";
+import { GLOBAL_ENVS, dappmanagerApiUrlGlobalEnvs } from "../params";
 import { config } from "../config";
 
 export async function getInternalIpCached(): Promise<string> {
   // internal IP is an optional feature for when NAT-Loopback is off
   try {
-    const internalIp = await got(GLOBAL_ENVS_KEYS.INTERNAL_IP, {
+
+    const envInternalIp = process.env[GLOBAL_ENVS.INTERNAL_IP];
+
+    if (envInternalIp && ip.isV4Format(envInternalIp)) {
+
+      logs.info(`Using internal IP from ENV: ${envInternalIp}`);
+
+      config.internalIp = envInternalIp;
+      return envInternalIp;
+    }
+
+    logs.info(`Fetching internal IP from DAPPMANAGER: ${dappmanagerApiUrlGlobalEnvs}`);
+
+    const internalIp = await got(GLOBAL_ENVS.INTERNAL_IP, {
       throwHttpErrors: true,
       prefixUrl: dappmanagerApiUrlGlobalEnvs
     })
