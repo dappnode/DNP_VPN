fee7dcbbeab0f5f4e542ec2e51d501220a285649
dappnodedev <diego@dappnode.io>
2023-12-26 16:30:30 +0100

    Add logging to hostname fetch

--- src/src/dappmanager/fetchHostname.ts
+++ src/src/dappmanager/fetchHostname.ts
@@ -3,11 +3,11 @@ import ip from "ip";
 import retry from "async-retry";
 import {
   dappmanagerApiUrlGlobalEnvs,
-  GLOBAL_ENVS_KEYS,
   GLOBAL_ENVS,
   NO_HOSTNAME_RETURNED_ERROR
 } from "../params";
 import { isDomain } from "../utils/domain";
+import { logs } from "../logs";
 
 /**
  * Polls the DAPPMANAGER to get the HOSTNAME (domain or IP) necessary to start
@@ -22,12 +22,18 @@ export async function fetchHostname({
 }): Promise<string> {
   // If ENVs are already available, do not poll
   const hostNameFromEnv = process.env[GLOBAL_ENVS.HOSTNAME];
-  if (hostNameFromEnv) return hostNameFromEnv;
+
+  if (hostNameFromEnv) {
+    logs.info(`Using hostname from ENV: ${hostNameFromEnv}`);
+    return hostNameFromEnv;
+  }
+
+  logs.info(`Fetching hostname from DAPPMANAGER: ${dappmanagerApiUrlGlobalEnvs}`);
 
   // Add async-retry in case the DAPPMANAGER returns an 200 code with empty hostname
   return await retry(
     () =>
-      got(GLOBAL_ENVS_KEYS.HOSTNAME, {
+      got(GLOBAL_ENVS.HOSTNAME, {
         throwHttpErrors: true,
         prefixUrl: dappmanagerApiUrlGlobalEnvs,
         retry: 10, // Should be about 15 minutes
