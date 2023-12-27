a478cd92366e20495032fa830ddf29d8b658ed44
dappnodedev <diego@dappnode.io>
2023-12-27 11:53:14 +0100

    Update params

--- src/src/params.ts
+++ src/src/params.ts
@@ -1,14 +1,14 @@
 import path from "path";
 
 // DAPPMANAGER Params
-export const dappmanagerApiUrl = "http://172.33.1.7";
+export const dappmanagerApiUrl = "http://dappmanager.dappnode";
 export const dappmanagerApiUrlGlobalEnvs = `${dappmanagerApiUrl}/global-envs`;
 
 // OpenVPN parameters
 export const USER_LIMIT = 500;
 export const MAIN_ADMIN_NAME = "dappnode_admin";
-export const MASTER_ADMIN_IP = "172.33.10.1";
-export const ADMIN_IP_RANGE = ["172.33.10.2", "172.33.11.250"];
+export const MASTER_ADMIN_IP = "10.20.0.129";
+export const ADMIN_IP_RANGE = ["10.20.0.130", "10.20.0.159"];
 
 // Paths
 export const TOKENS_DB_PATH = "/usr/src/app/secrets/tokens-db.json";
@@ -27,29 +27,8 @@ export const CRED_URL_PATHNAME = "/cred";
 
 // Global ENVs names
 export const GLOBAL_ENVS = {
-  ACTIVE: "_DAPPNODE_GLOBAL_ENVS_ACTIVE",
-  DOMAIN: "_DAPPNODE_GLOBAL_DOMAIN", // "" || "6b3d49d4965584c2.dyndns.dappnode.io"
-  STATIC_IP: "_DAPPNODE_GLOBAL_STATIC_IP", // "" || "138.68.106.96"
   HOSTNAME: "_DAPPNODE_GLOBAL_HOSTNAME", // "6b3d49d4965584c2.dyndns.dappnode.io" || "138.68.106.96"
-  INTERNAL_IP: "_DAPPNODE_GLOBAL_INTERNAL_IP", // "192.168.0.1"
-  UPNP_AVAILABLE: "_DAPPNODE_GLOBAL_UPNP_AVAILABLE", // "true" || "false"
-  NO_NAT_LOOPBACK: "_DAPPNODE_GLOBAL_NO_NAT_LOOPBACK", // "true" || "false"
-  PUBKEY: "_DAPPNODE_GLOBAL_PUBKEY", // "0x6B3D49d4965584C28Fbf14B82b1012664a73b9Ab"
-  PUBLIC_IP: "_DAPPNODE_GLOBAL_PUBLIC_IP", // "138.68.106.96"
-  SERVER_NAME: "_DAPPNODE_GLOBAL_SERVER_NAME" // "MyDAppNode"
-};
-
-export const GLOBAL_ENVS_KEYS: { [K in keyof typeof GLOBAL_ENVS]: K } = {
-  ACTIVE: "ACTIVE",
-  DOMAIN: "DOMAIN",
-  STATIC_IP: "STATIC_IP",
-  HOSTNAME: "HOSTNAME",
-  INTERNAL_IP: "INTERNAL_IP",
-  UPNP_AVAILABLE: "UPNP_AVAILABLE",
-  NO_NAT_LOOPBACK: "NO_NAT_LOOPBACK",
-  PUBKEY: "PUBKEY",
-  PUBLIC_IP: "PUBLIC_IP",
-  SERVER_NAME: "SERVER_NAME"
+  INTERNAL_IP: "_DAPPNODE_GLOBAL_INTERNAL_IP" // "192.168.0.1"
 };
 
 // Internal error messages
