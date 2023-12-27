9c418648b8fce4519af91f77d850c3992af51cc4
dappnodedev <diego@dappnode.io>
2023-12-27 11:53:43 +0100

    Resolve admin IPs

--- src/src/api/auth.ts
+++ src/src/api/auth.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import { logs } from "../logs";
+import { promisify } from "util";
+import dns from "dns";
+
+const resolveIp = promisify(dns.resolve4);
 
 const allowAllIps = Boolean(process.env.ALLOW_ALL_IPS);
 
@@ -7,15 +11,15 @@ if (allowAllIps) logs.warn(`WARNING! ALLOWING ALL IPFS`);
 
 // Authorize by IP
 
+const adminDomains = ["dappmanager.dappnode", "wifi.dappnode"];
+// Necessary in case the domain cannot be resolved
+const fallbackDomainIps = ["10.20.0.10", "10.20.0.7"];
+
 const adminIps = [
   // Admin users connecting from the VPN
-  "172.33.10.",
+  "10.20.10.",
   // Admin users connecting from the WIFI
-  "172.33.12.",
-  // WIFI DNP ip, which may be applied to users in some situations
-  "172.33.1.10",
-  // DAPPMANAGER IP
-  "172.33.1.7",
+  "10.20.12.",
   // Also localhost calls
   "127.0.0.1"
 ];
@@ -24,6 +28,19 @@ const localhostIps = [
   // Internal calls from the same container
   "127.0.0.1"
 ];
+
+// Needs to be called on startup
+export async function resolveAdminIps() {
+  try {
+    const resolvedIps = await Promise.all(adminDomains.map(domain => resolveIp(domain)));
+    resolvedIps.flat().forEach(ip => adminIps.push(ip));
+    logs.info(`Resolved admin domains (including dappmanager and wifi): ${adminIps}`);
+  } catch (error) {
+    logs.error(`Error resolving admin domains (dappmanager and wifi): ${error}`);
+    logs.warn(`Falling back to default IPs: ${fallbackDomainIps}`);
+    adminIps.push(...fallbackDomainIps);
+  }
+}
 
 function isAdminIp(ip: string): boolean {
   return allowAllIps || adminIps.some(_ip => ip.includes(_ip));
